954b9cf4f56448385ae7dc43ba72fee01c02e4c7
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-27 05:08:51 +0300

    up

--- routes.js
+++ routes.js
@@ -1,7 +1,17 @@
 const path = require("path");
 const dotenv = require("dotenv");
-const { register, generateToken, apply } = require("./controller/auth");
+const {
+  register,
+  generateToken,
+  apply,
+  getData,
+
+  adminLogin,
+  createRoom,
+  joinRoom,
+} = require("./controller/auth");
 const multer = require("multer");
+const { isUser } = require("./middleware/auth");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
@@ -15,11 +25,23 @@ const upload = multer({ storage: storage });
 dotenv.config();
 
 module.exports = function (app) {
-  app.get("/", async (req, res) => {
-    res.send("<p>Hello World!!</p>");
-  });
-
   app.post("/register", upload.single("myfile"), register);
   app.get("/generate-token", generateToken);
   app.post("/apply", upload.single("myfile"), apply);
+  app.post("/adminLogin", adminLogin);
+  app.get("/checkToken", isUser, getData);
+  app.post("/createRoom", isUser, createRoom);
+  app.post("/joinRoom", joinRoom);
+
+  // app.get("/download/:filename", (req, res) => {
+  //   const filename = req.params.filename;
+  //   const filePath = path.join(__dirname, "uploads", filename);
+
+  //   res.download(filePath, (err) => {
+  //     if (err) {
+  //       res.setHeader("error", "File not found");
+  //       return res.status(404).send("File not found");
+  //     }
+  //   });
+  // });
 };
